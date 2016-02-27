92815f7d7ff8be3b487f75ad4ec9491298e9efc6
Engine <engine@madadeMacBook-Pro.local>
2016-02-27 18:14:21 +0800

    add pages

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 use Org\Net\Http;
 use Org\Net\Snoopy;
+use Think\Page;
 use Org\Net\simple_html_dom;
 class IndexController extends Controller {
 
@@ -15,10 +16,18 @@ class IndexController extends Controller {
 
     public function index(){
 
-        $sql = "select * from movie_data order by rank desc limit 20";
-        $data = $this->movieModel->query($sql);
-        $this->assign('list',$data);
-        $this->display();
+        $count      = $this->movieModel->count();//查询满足要求的总记录数
+        $Page       = new Page($count,8);// 实例化分页类 传入总记录数和每页显示的记录数
+        $show       = $Page->show();// 分页显示输出
+
+        //进行分页数据查询 注意limit方法的参数要使用Page类的属性
+        $list = $this->movieModel->order('rank desc')
+                ->limit($Page->firstRow.','.$Page->listRows)->select();
+
+        $this->assign('list',$list);// 赋值数据集
+        $this->assign('page',$show);// 赋值分页输出
+        $this->display(); // 输出模板
+
     }
 
     public function watchway(){
